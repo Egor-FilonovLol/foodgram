7f029d824424434782e2210d6479b96ef73edd4c
Egor <efilonov2@mail.ru>
2026-04-06 02:46:44 +0300

    fix2_after_review

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -20,16 +20,16 @@ class RecipeFilter(rest_framework.FilterSet):
         to_field_name="slug",
     )
     author = rest_framework.NumberFilter(field_name="author__id")
-    is_favorited = django_filters.filters.BooleanFilter(
+    is_favorited = rest_framework.BooleanFilter(
         method='is_recipe_in_favorites_filter')
-    is_in_shopping_cart = django_filters.filters.BooleanFilter(
+    is_in_shopping_cart = rest_framework.BooleanFilter(
         method='is_recipe_in_shopping_cart_filter')
 
     def is_recipe_in_shopping_cart_filter(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(
-                shopping_recipe__user=user.id
+                shopping_recipe__user=user
             )
         else:
             return queryset
