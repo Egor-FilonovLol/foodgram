5261da2e63566bfe17355658cdb517bd28064f7b
Egor <efilonov2@mail.ru>
2026-04-05 06:39:52 +0300

    fix1000

--- backend/users/models.py
+++ backend/users/models.py
@@ -25,5 +25,6 @@ class User(AbstractUser):
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
         ordering = ("username",)
+
     def __str__(self):
         return self.username
