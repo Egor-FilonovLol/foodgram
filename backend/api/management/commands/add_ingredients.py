49a11c788be5f864d15bc7de03fba40f27badf40
Egor <efilonov2@mail.ru>
2026-04-07 03:56:02 +0300

    fix1000

--- backend/api/management/commands/add_ingredients.py
+++ backend/api/management/commands/add_ingredients.py
@@ -1,5 +1,7 @@
 import csv
+from pathlib import Path
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from recipes.models import Ingredient
 
 
@@ -7,10 +9,9 @@ class Command(BaseCommand):
     help = 'Загрузка ингредиентов из CSV'
 
     def handle(self, *args, **kwargs):
-        path = 'data/ingredients.csv'
+        path = Path(settings.BASE_DIR).parent / 'data' / 'ingredients.csv'
         with open(path, encoding='utf-8') as f:
             reader = csv.reader(f)
-            next(reader)
             ingredients = [
                 Ingredient(name=row[0], measurement_unit=row[1])
                 for row in reader
