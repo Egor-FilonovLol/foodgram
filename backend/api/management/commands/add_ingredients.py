c8dec4ddc7097656e8d2b2efbe91f6c0d2196b87
Egor <efilonov2@mail.ru>
2026-04-07 21:26:36 +0300

    fix1000

--- backend/api/management/commands/add_ingredients.py
+++ backend/api/management/commands/add_ingredients.py
@@ -12,10 +12,9 @@ class Command(BaseCommand):
         path = os.path.join(settings.BASE_DIR, 'data', 'ingredients.csv')
         with open(path, encoding='utf-8') as f:
             reader = csv.reader(f)
-            next(reader)
             ingredients = [
                 Ingredient(name=row[0], measurement_unit=row[1])
                 for row in reader
             ]
-            Ingredient.objects.bulk_create(ingredients)
+            Ingredient.objects.bulk_create(ingredients, ignore_conflicts=True)
         self.stdout.write(self.style.SUCCESS('готово! ингредиенты загружены.'))
