566dfc8cc03deac2b7f930938f3b43175b4ae457
Egor-FilonovLol <egor.filonov.2017@mail.ru>
2026-04-08 15:25:28 +0300

    fix1000

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,8 +5,11 @@ from django_filters import rest_framework
 from recipes.models import Recipe, Tag, Ingredient
 
 
-class IngredientFilter(FilterSet):
-    name = django_filters.CharFilter(field_name="name", lookup_expr="istartswith")
+class IngredientFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name="name",
+        lookup_expr="istartswith"
+    )
 
     class Meta:
         model = Ingredient
